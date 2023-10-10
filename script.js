d95a1138803df218a4f2fc06f70c3f2a04964f81
Ilyos <ilyosshaydullaye212@gmail.com>
2023-10-10 17:54:21 +0500

    UI completed

---
+++ script.js
@@ -0,0 +1,9 @@
+const fullname=document.querySelector('.full-name')
+const usernamereg=document.querySelector('.usernamereg')
+const passwordreg=document.querySelector('.passwordreg')
+const registerbtn=document.querySelector('.register')
+const usernamelog=document.querySelector('.usernamelog')
+const passwordlog=document.querySelector('.passwordlog')
+const loginbtn=document.querySelector('.login')
+
+
